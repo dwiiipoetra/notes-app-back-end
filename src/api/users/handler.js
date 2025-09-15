754a1363ec754ec9f4830056c5da78b42829edd9
dwiiipoetra2 <dwi.waluyoputranto@gmail.com>
2025-09-15 23:39:30 +0700

    first commit

---
+++ src/api/users/handler.js
@@ -0,0 +1,78 @@
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+    // this.getNotesHandler = this.getNotesHandler.bind(this);
+    // this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
+    // this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
+  }
+
+  async postUserHandler(request, h) {
+    this._validator.validateUserPayload(request.payload);
+    const { username, password, fullname } = request.payload;
+
+    const userId = await this._service.addUser({
+      username,
+      password,
+      fullname,
+    });
+
+    const response = h.response({
+      status: "success",
+      message: "User berhasil ditambahkan",
+      data: {
+        userId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
+    return {
+      status: "success",
+      data: {
+        notes,
+      },
+    };
+  }
+
+  async getUserByIdHandler(request, h) {
+    const { id } = request.params;
+    const user = await this._service.getUserById(id);
+    return {
+      status: "success",
+      data: {
+        user,
+      },
+    };
+  }
+
+  async putNoteByIdHandler(request, h) {
+    this._validator.validateNotePayload(request.payload);
+    const { id } = request.params;
+
+    await this._service.editNoteById(id, request.payload);
+
+    return {
+      status: "success",
+      message: "Catatan berhasil diperbarui",
+    };
+  }
+
+  async deleteNoteByIdHandler(request, h) {
+    const { id } = request.params;
+    await this._service.deleteNoteById(id);
+
+    return {
+      status: "success",
+      message: "Catatan berhasil dihapus",
+    };
+  }
+}
+
+module.exports = UsersHandler;
